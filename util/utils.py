285108b271e047430209369669e5ccc02a6ffe3c
Tennant <Tennant_1999@outlook.com>
2018-05-15 14:45:56 +0800

    add data split

--- util/utils.py
+++ util/utils.py
@@ -1,7 +1,8 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-
 def show_img(img):
+	import matplotlib.pyplot as plt
 	plt.imshow(img)
 	plt.show()
+
+def to_one_hot(values):
+	import numpy as np
+	return np.eye(values.max() + 1)[values]
