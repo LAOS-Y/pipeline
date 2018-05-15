1d891f129871a7da0e5078a2caaf055cc414fabb
Tennant <Tennant_1999@outlook.com>
2018-05-15 14:16:21 +0800

    add model averaging

--- processer/load_data.py
+++ processer/load_data.py
@@ -12,7 +12,9 @@ def load_and_resize(file_labels, file_path, resize_list=[(400, 400), (299, 299),
 			img_class = int(temp[1]) - 1
 			img_list.append([img_name, img_class])
 	indice_mask = np.random.permutation(len(img_list))
-	new_img_list = []
+	new_img_list = [[]]
+	for _ in resize_list:
+		new_img_list.append([])
 	label_list = []
 	for i in range(len(img_list)):
 		if (i + 1) % 500 == 0:
@@ -20,10 +22,11 @@ def load_and_resize(file_labels, file_path, resize_list=[(400, 400), (299, 299),
 		img_file = img_dir + img_list[indice_mask[i]][0]
 		img_class = img_list[indice_mask[i]][1]
 		img = Image.open(img_file)
-		temp_list = [img]
-		for size in resize_list:
+		#temp_list = [img]
+		new_img_list[0].append(np.asarray(img))
+		for i, size in enumerate(resize_list):
 			resized_img = img.resize(size)
-			temp_list.append(resized_img)
+			new_img_list[i + 1].append(np.asarray(resized_img))
 		label_list.append(img_class)
-		new_img_list.append(temp_list)
+		#new_img_list.append(temp_list)
 	return new_img_list, label_list
